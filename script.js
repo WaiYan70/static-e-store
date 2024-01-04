c295840d104abd78f53348fab6b1d7797bd90b60
Khant Wai Yan <52145576+WaiYan70@users.noreply.github.com>
2024-01-04 23:33:43 +0630

    Add Single Product Page, add Images Slider and add Image's detail

--- script.js
+++ script.js
@@ -124,5 +124,28 @@ const caurosel = new Swiper('.carouselbox', {
             centeredSlides: false,
         }
     }
+});
 
+// product image > page-single
+const thumbImage = new Swiper('.thumbnail-image', {
+    // loop: true,
+    direction: 'vertical',
+    spaceBetween: 15,
+    slidesPerView: 1,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+
+const mainImage = new Swiper('.main-image',{
+    loop: true,
+    autoHeight: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    thumbs: {
+        swiper: thumbImage,
+    },
 });
