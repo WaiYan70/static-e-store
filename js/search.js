bf0f4c24063fdf32e4a57b1933db137685c43124
Khant Wai Yan <52145576+WaiYan70@users.noreply.github.com>
2024-02-17 17:30:45 +0630

    Finish to modify the files and folders structure

--- js/search.js
+++ js/search.js
@@ -10,8 +10,10 @@ const search = () => {
             let textValue = match.textContent || match.innerHTML;
             if(textValue.toUpperCase().indexOf(searchbox) > -1){
                 product[i].style.display = "";
+                // product[i].style.opacity = "1";
             } else {
                 product[i].style.display = "none";
+                // product[i].style.opacity = "0";
             }
         }        
     }
